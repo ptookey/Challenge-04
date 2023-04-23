d7c6af754fb6e479847a485ebb51b914a0b311e7
tookey95 <petetookey@gmail.com>
2023-04-23 13:33:18 -0400

    Added High Score Screen and started JS

---
+++ script.js
@@ -0,0 +1,13 @@
+var header = document.getElementById("header");
+var vhs = document.getElementById("vhs");
+var startScreen = document.getElementById("startScreen");
+var questions = document.getElementById("questions");
+var resultScreen = document.getElementById("resultScreen");
+var highscoreScreen = document.getElementById("highscoreScreen");
+
+questions.style.display = "none";
+resultScreen.style.display = "none";
+header.style.display = "none";
+vhs.style.display = "none";
+startScreen.style.display = "none";
+
